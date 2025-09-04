431c788dfe486ac81f73f2776027990e31184ddc
salman <salman.shafiq@asdbd.com>
2025-09-04 17:21:46 +0600

    db initialized

--- HashiCorpIntegration/Program.cs
+++ HashiCorpIntegration/Program.cs
@@ -33,6 +33,3 @@ app.MapControllerRoute(
 
 app.Run();
 
-// Add link to Tailwind CSS
-var link = "<link href=\"https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css\" rel=\"stylesheet\">";
-app.MapGet("/tailwindcss", () => link).AllowAnonymous();
